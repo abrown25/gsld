28e79afc6c10bd00da4fea499f5a3c5cf30c6afd
Andrew Brown <aabrown24@hotmail.com>
2016-01-08 17:18:09 +0100

    formatting and unittest for cdf

--- source/gsl/vector_double.d
+++ source/gsl/vector_double.d
@@ -22,7 +22,7 @@ module gsl.vector_double;
 import gsl.block_double;
 import core.stdc.stdio;
 
-import unit_threaded: Name;
+import unit_threaded : Name;
 
 extern (C)
 {
@@ -30,8 +30,8 @@ extern (C)
   {
     size_t size;
     size_t stride;
-    double *data;
-    gsl_block *block;
+    double* data;
+    gsl_block* block;
     int owner;
   }
 
@@ -49,116 +49,114 @@ extern (C)
 
   alias gsl_vector_const_view = const _gsl_vector_const_view;
 
+  /* Allocation */
 
-/* Allocation */
+  gsl_vector* gsl_vector_alloc(const size_t n);
+  gsl_vector* gsl_vector_calloc(const size_t n);
 
-  gsl_vector *gsl_vector_alloc (const size_t n);
-  gsl_vector *gsl_vector_calloc (const size_t n);
+  gsl_vector* gsl_vector_alloc_from_block(gsl_block* b, const size_t offset,
+    const size_t n, const size_t stride);
 
-  gsl_vector *gsl_vector_alloc_from_block (gsl_block * b,
-					   const size_t offset, 
-					   const size_t n, 
-					   const size_t stride);
+  gsl_vector* gsl_vector_alloc_from_vector(gsl_vector* v, const size_t offset,
+    const size_t n, const size_t stride);
 
-  gsl_vector *gsl_vector_alloc_from_vector (gsl_vector * v,
-					    const size_t offset, 
-					    const size_t n, 
-					    const size_t stride);
+  void gsl_vector_free(gsl_vector* v);
 
-  void gsl_vector_free (gsl_vector * v);
+  /* Views */
 
-/* Views */
+  _gsl_vector_view gsl_vector_view_array(double* v, size_t n);
 
-  _gsl_vector_view 
-    gsl_vector_view_array (double *v, size_t n);
+  _gsl_vector_view gsl_vector_view_array_with_stride(double* base, size_t stride, size_t n);
 
-  _gsl_vector_view 
-    gsl_vector_view_array_with_stride (double *base,
-				       size_t stride,
-				       size_t n);
+  _gsl_vector_const_view gsl_vector_const_view_array(const double* v, size_t n);
 
-  _gsl_vector_const_view 
-    gsl_vector_const_view_array (const double *v, size_t n);
+  _gsl_vector_const_view gsl_vector_const_view_array_with_stride(const double* base,
+    size_t stride, size_t n);
 
-  _gsl_vector_const_view 
-    gsl_vector_const_view_array_with_stride (const double *base,
-					     size_t stride,
-					     size_t n);
+  _gsl_vector_view gsl_vector_subvector(gsl_vector* v, size_t i, size_t n);
 
-  _gsl_vector_view 
-    gsl_vector_subvector (gsl_vector *v, 
-			  size_t i, 
-			  size_t n);
+  _gsl_vector_view gsl_vector_subvector_with_stride(gsl_vector* v, size_t i, size_t stride,
+    size_t n);
 
-  _gsl_vector_view 
-    gsl_vector_subvector_with_stride (gsl_vector *v, 
-				      size_t i,
-				      size_t stride,
-				      size_t n);
+  _gsl_vector_const_view gsl_vector_const_subvector(const gsl_vector* v, size_t i,
+    size_t n);
 
-  _gsl_vector_const_view 
-    gsl_vector_const_subvector (const gsl_vector *v, 
-				size_t i, 
-				size_t n);
+  _gsl_vector_const_view gsl_vector_const_subvector_with_stride(const gsl_vector* v,
+    size_t i, size_t stride, size_t n);
 
-  _gsl_vector_const_view 
-    gsl_vector_const_subvector_with_stride (const gsl_vector *v, 
-					    size_t i, 
-					    size_t stride,
-					    size_t n);
+  /* Operations */
 
-/* Operations */
+  void gsl_vector_set_zero(gsl_vector* v);
+  void gsl_vector_set_all(gsl_vector* v, double x);
+  int gsl_vector_set_basis(gsl_vector* v, size_t i);
 
-  void gsl_vector_set_zero (gsl_vector * v);
-  void gsl_vector_set_all (gsl_vector * v, double x);
-  int gsl_vector_set_basis (gsl_vector * v, size_t i);
+  int gsl_vector_fread(FILE* stream, gsl_vector* v);
+  int gsl_vector_fwrite(FILE* stream, const gsl_vector* v);
+  int gsl_vector_fscanf(FILE* stream, gsl_vector* v);
+  int gsl_vector_fprintf(FILE* stream, const gsl_vector* v, const char* format);
 
-  int gsl_vector_fread (FILE * stream, gsl_vector * v);
-  int gsl_vector_fwrite (FILE * stream, const gsl_vector * v);
-  int gsl_vector_fscanf (FILE * stream, gsl_vector * v);
-  int gsl_vector_fprintf (FILE * stream, const gsl_vector * v,
-			  const char *format);
+  int gsl_vector_memcpy(gsl_vector* dest, const gsl_vector* src);
 
-  int gsl_vector_memcpy (gsl_vector * dest, const gsl_vector * src);
+  int gsl_vector_reverse(gsl_vector* v);
 
-  int gsl_vector_reverse (gsl_vector * v);
+  int gsl_vector_swap(gsl_vector* v, gsl_vector* w);
+  int gsl_vector_swap_elements(gsl_vector* v, const size_t i, const size_t j);
 
-  int gsl_vector_swap (gsl_vector * v, gsl_vector * w);
-  int gsl_vector_swap_elements (gsl_vector * v, const size_t i, const size_t j);
+  double gsl_vector_max(const gsl_vector* v);
+  double gsl_vector_min(const gsl_vector* v);
+  void gsl_vector_minmax(const gsl_vector* v, double* min_out, double* max_out);
 
-  double gsl_vector_max (const gsl_vector * v);
-  double gsl_vector_min (const gsl_vector * v);
-  void gsl_vector_minmax (const gsl_vector * v, double * min_out, double * max_out);
+  size_t gsl_vector_max_index(const gsl_vector* v);
+  size_t gsl_vector_min_index(const gsl_vector* v);
+  void gsl_vector_minmax_index(const gsl_vector* v, size_t* imin, size_t* imax);
 
-  size_t gsl_vector_max_index (const gsl_vector * v);
-  size_t gsl_vector_min_index (const gsl_vector * v);
-  void gsl_vector_minmax_index (const gsl_vector * v, size_t * imin, size_t * imax);
+  int gsl_vector_add(gsl_vector* a, const gsl_vector* b);
+  int gsl_vector_sub(gsl_vector* a, const gsl_vector* b);
+  int gsl_vector_mul(gsl_vector* a, const gsl_vector* b);
+  int gsl_vector_div(gsl_vector* a, const gsl_vector* b);
+  int gsl_vector_scale(gsl_vector* a, const double x);
+  int gsl_vector_add_constant(gsl_vector* a, const double x);
 
-  int gsl_vector_add (gsl_vector * a, const gsl_vector * b);
-  int gsl_vector_sub (gsl_vector * a, const gsl_vector * b);
-  int gsl_vector_mul (gsl_vector * a, const gsl_vector * b);
-  int gsl_vector_div (gsl_vector * a, const gsl_vector * b);
-  int gsl_vector_scale (gsl_vector * a, const double x);
-  int gsl_vector_add_constant (gsl_vector * a, const double x);
+  int gsl_vector_equal(const gsl_vector* u, const gsl_vector* v);
 
-  int gsl_vector_equal (const gsl_vector * u, 
-			const gsl_vector * v);
+  int gsl_vector_isnull(const gsl_vector* v);
+  int gsl_vector_ispos(const gsl_vector* v);
+  int gsl_vector_isneg(const gsl_vector* v);
+  int gsl_vector_isnonneg(const gsl_vector* v);
 
-  int gsl_vector_isnull (const gsl_vector * v);
-  int gsl_vector_ispos (const gsl_vector * v);
-  int gsl_vector_isneg (const gsl_vector * v);
-  int gsl_vector_isnonneg (const gsl_vector * v);
+  double gsl_vector_get(const gsl_vector* v, const size_t i);
+  void gsl_vector_set(gsl_vector* v, const size_t i, double x);
+  double* gsl_vector_ptr(gsl_vector* v, const size_t i);
+  double* gsl_vector_const_ptr(const gsl_vector* v, const size_t i);
 
-  double gsl_vector_get (const gsl_vector * v, const size_t i);
-  void gsl_vector_set (gsl_vector * v, const size_t i, double x);
-  double * gsl_vector_ptr (gsl_vector * v, const size_t i);
-  double * gsl_vector_const_ptr (const gsl_vector * v, const size_t i);
+  version (HAVE_INLINE)
+  {
+    double gsl_vector_get(const gsl_vector* v, const size_t i)
+    {
+      return v.data[i * v.stride];
+    }
 
+    void gsl_vector_set(gsl_vector* v, const size_t i, double x)
+    {
+      v.data[i * v.stride] = x;
+    }
+
+    double* gsl_vector_ptr(gsl_vector* v, const size_t i)
+    {
+      return cast(double*)(v.data + i * v.stride);
+    }
+
+    const double* gsl_vector_const_ptr(const gsl_vector* v, const size_t i)
+    {
+      return cast(const(double*))(v.data + i * v.stride);
+    }
+
+  }
 }
 
 gsl_vector* d_gsl_vector_double_set(double[] vector)
 {
-  gsl_vector * vec = gsl_vector_alloc(vector.length);
+  gsl_vector* vec = gsl_vector_alloc(vector.length);
 
   foreach (i; 0 .. vector.length)
   {
