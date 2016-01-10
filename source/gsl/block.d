1ffd7a13f428ca58a60398a6d6e19066769f5d5f
Chibisi Chima-Okereke <chibisi@active-analytics.com>
2016-01-10 22:02:55 +0000

    included gsl_vector and gsl_block header file

---
+++ source/gsl/block.d
@@ -0,0 +1,21 @@
+/*
+* D interface file:
+* Author: Chibisi Chima-Okereke
+*/
+module gsl.block;
+
+import gsl.block_char;
+import gsl.block_complex_double;
+import gsl.block_complex_float;
+import gsl.block_complex_long_double;
+import gsl.block_double;
+import gsl.block_float;
+import gsl.block_int;
+import gsl.block_long;
+import gsl.block_long_double;
+import gsl.block_short;
+import gsl.block_uchar;
+import gsl.block_uint;
+import gsl.block_ulong;
+import gsl.block_ushort;
+
